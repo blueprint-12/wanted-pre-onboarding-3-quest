be88e148da47afc175ec0e2d8a20ebd7145df93f
blueprint-12 <blueprint12@daum.net>
2023-03-09 22:19:06 +0900

    feat:  테스팅 mui 컴포넌트

---
+++ src/components/MuiTest.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Slider from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+
+const CustomizedSlider = styled(Slider)`
+  color: #20b2aa;
+  :hover {
+    color: #2e8b57;
+  }
+`;
+function MuiTest() {
+  return (
+    <>
+      <Slider defaultValue={30} />
+      <CustomizedSlider defaultValue={30} />
+    </>
+  );
+}
+
+export default Slider;
