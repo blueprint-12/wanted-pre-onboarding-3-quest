1afb79eb0cbd8190dcc4135ffc4ddfc8ad9a07fa
blueprint-12 <blueprint12@daum.net>
2023-03-15 23:46:37 +0900

    feat: Home, C 페이지 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { Button } from "@mui/material";
+import { useRouter } from "../hooks/useRouter";
 
 function Home() {
+  const { routeTo } = useRouter();
+
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <div className="non-logged-in-body">
+      <h1>Home!</h1>
+      <p>
+        로그인을 필요로 하지 않는 메인 페이지입니다. <br />
+        만약 로그인이 된 상태에서 접근했다면 Page A로 자동으로 이동시킵니다.{" "}
+        <br />
+        로그인 하러 가기 버튼을 아래에 구현합니다.
+      </p>
+      <div className="center">
+        <Button
+          variant="contained"
+          onClick={() => {
+            routeTo("login");
+          }}>
+          로그인 하러 가기
+        </Button>
+      </div>
+    </div>
   );
 }
 
